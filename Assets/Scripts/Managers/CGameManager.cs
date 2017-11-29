1cb874c48dc5a0253c6d54788654911051c7f288
Co Chi Tam <cochitam1990@gmail.com>
2017-11-29 13:51:25 +0700

    + Update models + New build.

---
+++ Assets/Scripts/Managers/CGameManager.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using SimpleSingleton;
+
+public class CGameManager : CMonoSingleton<CGameManager> {
+
+	protected override void Awake ()
+	{
+		base.Awake ();
+		DontDestroyOnLoad (this.gameObject);
+	}
+
+}
